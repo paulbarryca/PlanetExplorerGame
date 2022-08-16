6bb1a1b342d25e6398abad86121957c429049512
paulbtoronto <paulbtoronto@gmail.ca>
2022-08-16 15:28:28 -0400

    Initial Commit

---
+++ PlanetExplorerGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlanetExplorerGame
+//
+//  Created by PAUL BARRY on 2022-08-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlanetExplorerGame/PlanetExplorerGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetExplorerGameApp.swift
+//  PlanetExplorerGame
+//
+//  Created by PAUL BARRY on 2022-08-16.
+//
+
+import SwiftUI
+
+@main
+struct PlanetExplorerGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
